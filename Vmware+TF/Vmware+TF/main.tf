308ef0c402d3b457050db150140807502a1e4f31
thivakarjj <thivakar21@gmail.com>
2020-07-29 11:31:46 +0530

    commit

--- Vmware+TF/Vmware+TF/main.tf
+++ Vmware+TF/Vmware+TF/main.tf
@@ -138,11 +138,14 @@ inline = [
 ]
 }
 
+provisioner "remote-exec" {
+    connection {
+      type     = "winrm"
+      user     = "Administrator"
+      password = "${var.admin_password}"
+    }
 
-If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
-
-{   
-$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-Start-Process powershell -Verb runAs -ArgumentList $arguments
-Break
-}
+inline = [
+         "powershell -ExecutionPolicy Unrestricted -File C:\\ProgramData\\Amazon\\EC2-Windows\\Launch\\Scripts\\InitializeInstance.ps1 -Schedule"
+        ]
+      }
