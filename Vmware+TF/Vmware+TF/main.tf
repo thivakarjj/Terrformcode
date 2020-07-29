1d8a80a9b03af3efcabc170ecff91290e27248ee
thivakarjj <thivakar21@gmail.com>
2020-07-29 09:10:09 +0530

    commit

--- Vmware+TF/Vmware+TF/main.tf
+++ Vmware+TF/Vmware+TF/main.tf
@@ -151,24 +151,9 @@ Get-Disk -Number $i |Get-Partition |where {$_.type -eq "Basic"}|  Set-Partition 
 $j++
 }
 
-
-param([switch]$Elevated)
-
-function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
 }
-
-if ((Test-Admin) -eq $false)  {
-    if ($elevated) 
-    {
-        # tried to elevate, did not work, aborting
-    } 
-    else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-}
-
-exit
-}
-
-'running with full privileges'
